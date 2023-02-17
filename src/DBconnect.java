5345b270464fbcf27bd0694c1bb8d411bdc31622
M Raihan Aryadevin <93025188+MRaihan21@users.noreply.github.com>
2023-02-17 16:54:39 +0700

    hapus komentar

--- src/DBconnect.java
+++ src/DBconnect.java
@@ -19,17 +19,3 @@ public class DBconnect {
     
 }
 
-//Connection con = null;
-//
-//    public DBconnect() {
-//        try {
-//            Class.forName("com.mysql.cj.jdbc.Driver");
-//            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/dblogin","root ", "");                        
-//        } catch (Exception e) {
-//            JOptionPane.showMessageDialog(null, e);
-//        }
-//    }
-//
-//    public Connection getConn(){
-//        return con;
-//    }
