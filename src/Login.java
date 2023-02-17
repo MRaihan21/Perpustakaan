d7dc2036a96ba718ff5c16712c7d1fc4a0c23ea7
M Raihan Aryadevin <93025188+MRaihan21@users.noreply.github.com>
2023-02-17 16:55:35 +0700

    menambahkan kode untuk login dari database

--- src/Login.java
+++ src/Login.java
@@ -3,12 +3,18 @@ package Perpustakaan;
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
+import java.sql.*;
 
-class Login extends JFrame implements ActionListener {           
+class Login extends JFrame implements ActionListener {
+
+    Connection con;
+    Statement stmt;
+    ResultSet rs;
+    DBconnect koneksi;
 
     ImageIcon image;
-    JLabel judul, username, password;   
-    JTextField textUsername, textPassword;    
+    JLabel judul, username, password, register;
+    JTextField textUsername, textPassword;
     JButton button;
 
     Login() {
@@ -28,19 +34,19 @@ class Login extends JFrame implements ActionListener {
         judul = new JLabel();
         judul.setText("Aplikasi Perpustakaan");
         judul.setIcon(image);
-        
+
         judul.setForeground(Color.white);
         judul.setHorizontalTextPosition(JLabel.CENTER);
         judul.setVerticalTextPosition(JLabel.BOTTOM);
         judul.setHorizontalAlignment(JLabel.CENTER);
-        judul.setVerticalAlignment(JLabel.CENTER);               
+        judul.setVerticalAlignment(JLabel.CENTER);
 
         //username
         username = new JLabel();
         username.setText("Username");
         username.setBounds(130, 320, 90, 20);
         username.setForeground(Color.gray);
-        this.add(username);        
+        this.add(username);
 
         textUsername = new JTextField();
         textUsername.setBounds(115, 320, 160, 20);
@@ -51,46 +57,86 @@ class Login extends JFrame implements ActionListener {
         password.setText("Password");
         password.setBounds(130, 350, 90, 20);
         password.setForeground(Color.gray);
-        this.add(password);        
+        this.add(password);
 
         textPassword = new JPasswordField();
         textPassword.setBounds(115, 350, 160, 20);
         this.add(textPassword);
 
-        //button
-        button = new JButton();
-        button.setText("Login");
+        //button        
+        button = new JButton("Login");
         button.setForeground(Color.black);
         button.setFocusable(false);
-        button.setBounds(150, 380, 80, 20);
-        button.addActionListener(this);
         button.setBackground(Color.white);
+        button.setBounds(150, 380, 80, 20);
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                try {
+                    Class.forName("com.mysql.cj.jdbc.Driver");
+                    Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/dblogin", "root", "");
+                    Statement stm = con.createStatement();
+
+                    String username = textUsername.getText();
+                    String password = textPassword.getText();
+                    String sql = "SELECT * FROM tblogin WHERE username='"
+                            + username + "' AND password='" + password + "'";
+                    ResultSet rs = stm.executeQuery(sql);
+
+                    if (username.equals("") && password.equals("")) {
+                        JOptionPane.showMessageDialog(null, "Isi Username dan Password", "Message", JOptionPane.ERROR_MESSAGE);
+                    } else if (password.equals("")) {
+                        JOptionPane.showMessageDialog(null, "Isi Password", "Message", JOptionPane.ERROR_MESSAGE);
+                    } else if (username.equals("")) {
+                        JOptionPane.showMessageDialog(null, "Isi Username", "Message", JOptionPane.ERROR_MESSAGE);
+                    } else if (rs.next()) {
+                        JOptionPane.showMessageDialog(null, "Selamat Datang " + username);
+                        dispose();
+                        PageLibrary pagelibrary = new PageLibrary();
+                        pagelibrary.setVisible(true);
+                    } else {
+                        JOptionPane.showMessageDialog(null, "Username & Password Salah", "Message", JOptionPane.ERROR_MESSAGE);
+                    }
+
+                } catch (Exception ex) {
+                    System.out.println(ex.getMessage());
+                }
+
+            }
+        });
         this.add(button);
 
+        register = new JLabel();
+        register.setText("Register a new account");
+        register.setBounds(130, 415, 180, 15);
+        register.setForeground(Color.red);
+
+        register.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+        register.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                dispose();
+                Register regis = new Register();
+                regis.setVisible(true);
+            }
+        });
+
+        this.add(register);
+
         this.add(judul);
-        this.getContentPane().setBackground(Color.ORANGE);        
+        this.getContentPane().setBackground(Color.ORANGE);
 
         //       
         this.setResizable(false);
         this.setVisible(true);
 
+        con = DBconnect.getConnection();
+
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-
-        if (e.getSource() == button) {
-            String username = "admin";
-            String password = "pass";
-            if (textUsername.getText().equals(username) && textPassword.getText().equals(password)) {
-                PageLibrary pageLibrary = new PageLibrary();
-                pageLibrary.setVisible(true);
-                this.dispose();
-            } else {
-                JOptionPane.showMessageDialog(null, "Username dan Password Salah", "Error", JOptionPane.ERROR_MESSAGE);                
-            }
-
-        }
 
     }
 
